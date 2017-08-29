d35e08b7db5a9068a10b772156844c3039db5c66
Aditya <zeandcode@gmail.com>
2017-08-29 11:52:39 +0700

    Added database and model configurations

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
-var restify = require('restify');
+'use strict';
+
+let path = require('path')
+global.app_root = path.resolve(__dirname)
+
+require('dotenv').config({
+  path: '.env',
+  encoding: 'utf8'
+})
+
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development';
+}
+
+const restify = require('restify')
+const corsMiddleware = require('restify-cors-middleware')
 
 const server = restify.createServer({
   name: 'pendekin',
   version: '1.0.0'
-});
+})
 
-server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.queryParser());
-server.use(restify.plugins.bodyParser());
+const cors = corsMiddleware({
+  preflightMaxAge: 5, //Optional
+  origins: ['http://api.myapp.com', 'http://web.myapp.com'],
+  allowHeaders: ['API-Token'],
+  exposeHeaders: ['API-Token-Expiry']
+})
+
+
+server.pre(restify.pre.sanitizePath());
+server.pre(cors.preflight)
+
+server.use(restify.plugins.gzipResponse())
+server.use(restify.plugins.jsonBodyParser({ mapParams: true }))
+server.use(restify.plugins.acceptParser(server.acceptable))
+server.use(restify.plugins.queryParser({ mapParams: true }))
+server.use(restify.plugins.fullResponse())
+server.use(restify.plugins.conditionalRequest()) // ETag support 
+server.use(cors.actual)
 
 server.get('/echo/:name', function (req, res, next) {
   res.send(req.params);
