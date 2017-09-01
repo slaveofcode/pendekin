3b05d396d7263963b8fc73198d7b2dfef2450876
Aditya <zeandcode@gmail.com>
2017-09-02 00:19:50 +0700

    Added migration for table `shorten_categories`

---
+++ migrations/20170901165547-create-table-shorten-categories.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const TABLE_NAME = 'shorten_categories'
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable(TABLE_NAME, {
+      id: {
+        type: Sequelize.UUID,
+        defaultvalue: Sequelize.UUIDV4,
+        primaryKey: true
+      },
+      name: Sequelize.STRING,
+      description: Sequelize.TEXT,
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true
+      }
+    })
+    .then(() => {
+      return Promise.all([
+        queryInterface.addIndex(TABLE_NAME, ['name']),
+        queryInterface.addIndex(TABLE_NAME, ['created_at']),
+        queryInterface.addIndex(TABLE_NAME, ['updated_at']),
+        queryInterface.addIndex(TABLE_NAME, ['deleted_at'])
+      ])
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable(TABLE_NAME);
+  }
+};
