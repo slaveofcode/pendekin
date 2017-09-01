a0b5bd2a5d6b0d272e68a269e1f3609c36215e11
Aditya <zeandcode@gmail.com>
2017-09-02 00:19:58 +0700

    Added migration for table `shorten_urls`

---
+++ migrations/20170901165823-create-table-shorten-urls.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const TABLE_NAME = 'shorten_urls'
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable(TABLE_NAME, {
+      id: {
+        type: Sequelize.UUID,
+        defaultvalue: Sequelize.UUIDV4,
+        primaryKey: true
+      },
+      category: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'shorten_categories',
+          key: 'id'
+        }
+      },
+      prefix: {
+        type: Sequelize.STRING(5),
+        allowNull: true
+      },
+      suffix: {
+        type: Sequelize.STRING(5),
+        allowNull: true
+      },
+      code: {
+        type: Sequelize.STRING(15),
+        allowNull: false
+      },
+      protected_password: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      parent_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: TABLE_NAME,
+          key: 'id'
+        },
+        allowNull: true
+      },
+      is_index_urls: {
+        type: Sequelize.BOOLEAN,
+        defaultvalue: false
+      },
+      is_auto_remove_on_visited: {
+        type: Sequelize.BOOLEAN,
+        defaultvalue: false
+      },
+      expired_at: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true
+      }
+    })
+    .then(() => {
+      return Promise.all([
+        queryInterface.addIndex(TABLE_NAME, ['category']),
+        queryInterface.addIndex(TABLE_NAME, ['prefix']),
+        queryInterface.addIndex(TABLE_NAME, ['suffix']),
+        queryInterface.addIndex(TABLE_NAME, ['code']),
+        queryInterface.addIndex(TABLE_NAME, ['protected_password']),
+        queryInterface.addIndex(TABLE_NAME, ['parent_id']),
+        queryInterface.addIndex(TABLE_NAME, ['is_index_urls']),
+        queryInterface.addIndex(TABLE_NAME, ['is_auto_remove_on_visited']),
+        queryInterface.addIndex(TABLE_NAME, ['expired_at']),
+        queryInterface.addIndex(TABLE_NAME, ['created_at']),
+        queryInterface.addIndex(TABLE_NAME, ['updated_at']),
+        queryInterface.addIndex(TABLE_NAME, ['deleted_at'])
+      ])
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable(TABLE_NAME);
+  }
+};
