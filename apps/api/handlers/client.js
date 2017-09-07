fb7101dd095003edb4bccc8d8d8976d29f9bf605
Aditya <zeandcode@gmail.com>
2017-09-07 14:57:42 +0700

    Create CRUD operations on AuthClient

--- apps/api/handlers/client.js
+++ apps/api/handlers/client.js
@@ -1,6 +1,8 @@
 'use strict'
 
+const _ = require('lodash')
 const Routing = require('restify-routing')
+const HttpStatus = require('http-status-codes')
 const RestifyError = require('restify-errors')
 const Permission = require('../utils/permission')
 const Joi = require(`${app_root}/libs/joi`);
@@ -10,10 +12,46 @@ const router = new Routing()
 
 const pageExtractor = Pagination()
 const schema = Joi.object().keys({
-  name: Joi.string().trim().required()
+  name: Joi.string().trim().required(),
+  client_key: Joi.string().trim().optional(),
+  client_secret: Joi.string().trim().optional(),
+  active: Joi.boolean().default(true).optional()
 })
 
-router.get('/', Permission.BasicOrClient(), (req, res, next) => {
+router.get('/', Permission.BasicOrClient(), async (req, res, next) => {
+  const { params } = req
+  const pageParams = pageExtractor(params)
+
+  try {
+    const clients = await DB.AuthClient.findAndCountAll(pageParams)
+    res.send({
+      rows: clients.rows,
+      payload: Object.assign({
+        count: clients.count
+      }, pageParams)
+    })
+    return next(err)
+  } catch (err) {
+    return next(err)
+  }
+})
+
+router.get('/:id', Permission.BasicOrClient(), async (req, res, next) => {
+  const { params } = req
+
+  try {
+    const client = await DB.AuthClient.findOne({
+      where: {
+        id: {
+          $eq: params.id
+        }
+      }
+    })
+    res.send(client)
+    return next(err)
+  } catch (err) {
+    return next(err)
+  }
 })
 
 router.post('/', Permission.BasicOrClient(), async (req, res, next) => {
@@ -27,23 +65,50 @@ router.post('/', Permission.BasicOrClient(), async (req, res, next) => {
       name: validatedParams.name
     })
     
-    res.send(client.toJSON())
+    res.send(client)
     return next()
   } catch (err) {
     return next(err)
   }
 })
 
-router.put('/', Permission.BasicOrClient(), (req, res, next) => {
+router.put('/:id', Permission.BasicOrClient(), async (req, res, next) => {
+  const { params } = req
 
+  try {
+    const postParams = _.omit(params, ['id'])
+    const validatedParams = await Joi.validate(postParams, schema)
+    const cleanParams = _.pick(validatedParams, Object.keys(postParams))
+    const client = await DB.AuthClient.findOne({
+      where: {
+        id: {
+          $eq: params.id
+        }
+      }
+    })
+
+    client.update(validatedParams)
+    res.send(client)
+    return next()
+  } catch (err) {
+    return next(err)
+  }
 })
 
-router.delete('/', Permission.BasicOrClient(), (req, res, next) => {
+router.delete('/:id', Permission.BasicOrClient(), async (req, res, next) => {
+  const { params } = req
 
+  try {
+    const client = await DB.AuthClient.destroy({
+      where: {
+        id: { $eq: params.id }
+      }
+    })
+    res.send(HttpStatus.NO_CONTENT)
+    return next()
+  } catch (err){
+    return next(err)
+  }
 })
 
 module.exports = router
-
-/**
- * TODO: CRUD client
- */
