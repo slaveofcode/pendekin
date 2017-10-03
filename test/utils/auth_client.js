7b3080417bdb57cb4a0cbacee211535a7800d65a
Aditya <zeandcode@gmail.com>
2017-10-03 17:15:52 +0700

    Added authclient util

---
+++ test/utils/auth_client.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const RandomString = require('randomstring')
+const DB = require(`${app_root}/models`)
+const UUIDV4 = require('uuid/v4')
+
+const create = async () => {
+  return DB.AuthClient.create({
+    id: UUIDV4(),
+      name: 'WebApp',
+      client_key: RandomString.generate({ length: 50 }),
+      client_secret: RandomString.generate({ length: 100 }),
+      active: true,
+      created_at: new Date(),
+      updated_at: new Date()
+  })
+}
+
+const getAuthorizationKey = async () => {
+  const authClient = await create()
+  const encodedKey = new Buffer(`${authClient.client_key}:${authClient.client_secret}`).toString('base64')
+  return encodedKey
+}
+
+module.exports = {
+  create,
+  getAuthorizationKey
+}
