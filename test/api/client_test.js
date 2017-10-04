54784197c13b71281366abe0f9da134e1819fdef
Aditya <zeandcode@gmail.com>
2017-10-04 15:50:32 +0700

    Preparing client test

---
+++ test/api/client_test.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const chai = require('chai')
+const faker = require('faker')
+const DB = require(`${app_root}/models`)
+const request = require('../utils/request')
+const authClient = require('../utils/auth_client')
+
+const expect = chai.expect
+
+describe('Client api\'s', () => {
+  describe('List', () => {})
+  describe('Edit', () => {})
+  describe('Detail', () => {})
+  describe('Create', () => {})
+  describe('Delete', () => {})
+})
